7ab0166bac53292f40b4bd9bd5ab46494954c2b5
Marcin Sydor <marcinsydor@gmail.com>
2019-06-17 22:42:04 +0100

    feat(page): add page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const NotFoundPage = () => (
+ 
+  <>
+
+
+      <h1>NOT FOUND</h1>
+      <p>Page does not exist.</p>
+      </>
+
+);
+
+export default NotFoundPage;
